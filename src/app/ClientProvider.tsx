7bb260a5d6ea1029b6b9c1ffa44c4e08e2c3e343
Fumigari <14913398+fumigari@users.noreply.github.com>
2025-06-21 22:53:24 -0300

    add contact

--- src/app/ClientProvider.tsx
+++ src/app/ClientProvider.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { Provider } from 'react-redux';
-import { store } from 'src/store';
+import { store } from '../store';
 
 export default function ClientProvider({
   children,
